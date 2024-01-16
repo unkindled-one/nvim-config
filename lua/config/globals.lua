1360e84a577ed1fd0ed609d3cb1a9f564bb5603d
Jason <jdowva@gmail.com>
2024-01-16 17:38:53 -0500

    Added oil as a the default file manager

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -2,3 +2,6 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.diagnostic.config({ virtual_text = false })
 vim.wo.relativenumber = true
+--- Fold stuff
+vim.o.foldcolumn = '1'
+vim.o.foldcolumn = '1'vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,12 @@
 return {
-	"folke/tokyonight.nvim",
+	-- "folke/tokyonight.nvim",
+  "catppuccin/nvim",
 	lazy = false,
 	priority = 1000,
 	opts = {},
 	config = function()
-		vim.cmd("colorscheme tokyonight-storm")
+		-- vim.cmd("colorscheme tokyonight-storm")
+		-- vim.cmd("colorscheme tokyonight-moon")
+    vim.cmd("colorscheme catppuccin-frappe")
 	end
 }
