f78491c94b3d65d2a9ef585d0ec7911cdc9ee27d
Jason <jdowva@gmail.com>
2024-05-30 23:29:54 -0400

    Changed keybinds V3

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -3,12 +3,15 @@ return {
   lazy = false,
   dependencies = { 'nvim-tree/nvim-web-devicons', 'folke/todo-comments.nvim' },
   keys = {
-   { '<leader>xx', '<cmd>TroubleToggle<CR>', desc = 'Open/close trouble list' },
-    { '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<CR>', desc = 'Open/close trouble list' },
-    { '<leader>xd', '<cmd>TroubleToggle document_diagnostics<CR>', desc = 'Open/close trouble list' },
-    { '<leader>xq', '<cmd>TroubleToggle quickfix<CR>', desc = 'Open trouble quickfix list' },
-    { '<leader>xl', '<cmd>TroubleToggle loclist<CR>', desc = 'Open trouble location list' },
-    { '<leader>xt', '<cmd>TodoTrouble<CR>', desc = 'Open todos in trouble' },
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics"
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics"
+    },
   },
-  opts = {}
 }
