f2b12bc7b23a192eef792f33532262a66adf849c
Jason Dow <jdowva@gmail.com>
2024-07-30 17:57:34 -0400

    Configured the sidebar plugin

--- lua/plugins/sidebar.lua
+++ lua/plugins/sidebar.lua
@@ -1,4 +1,14 @@
 return {
   'sidebar-nvim/sidebar.nvim',
   lazy = false,
+  config = function ()
+    require('sidebar-nvim').setup({
+      open = true,
+      datetime = {
+        icon = "󰥔",
+        format = "%a, %I:%M"
+      }
+    })
+    
+  end
 }
