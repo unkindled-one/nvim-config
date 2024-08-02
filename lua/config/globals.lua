bbd0ccb6419b714e66a9eb24fc1729b362ea9969
Jason Dow <jdowva@gmail.com>
2024-08-01 20:27:47 -0400

    Updated global.lua

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -7,12 +7,10 @@ vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 -- vim.opt.foldmethod = 'expr'
 -- vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 
+-- Stuff for powershell
 vim.o.shell = 'pwsh.exe'
 vim.o.shellcmdflag = '-NoLogo -NoProfile -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;$PSStyle.OutputRendering=[System.Management.Automation.OutputRendering]::PlainText;'
-
--- vim.o.shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
 vim.o.shellxquote = ''
--- vim.o.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
 vim.o.shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait'
 vim.o.shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
 vim.o.shellquote = ''
