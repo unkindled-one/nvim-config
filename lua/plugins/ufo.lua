2e784b5b13aa56a722b9e87271e6d1d376e21b35
Jason Dow <jdowva@gmail.com>
2024-09-08 19:46:16 -0400

    Added lsp configuration for ufo

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -11,6 +11,19 @@ return {
 
     vim.keymap.set("n", "zR", ufo.openAllFolds)
     vim.keymap.set("n", "zM", ufo.closeAllFolds)
+
+    -- setup lsp as provider
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true
+    }
+    local language_servers = require('lspconfig').util.available_servers()
+    for _, ls in ipairs(language_servers) do
+      require('lspconfig')[ls].setup({
+        capabilities = capabilities
+      })
+    end
     ufo.setup()
   end
 }
