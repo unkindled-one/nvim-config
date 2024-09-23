763bec0d6932b167c794003ff0b8c3107faafaaf
Jason Dow <jdowva@gmail.com>
2024-09-22 20:40:49 -0400

    Forgot to call setup on nvim-surround

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,5 +1,8 @@
 return {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
+  "kylechui/nvim-surround",
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
+  config = function ()
+    require('nvim-surround').setup({})
+  end
 }
