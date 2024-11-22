d8c2d1fda7336272bc39d51509fae57b2d8763ef
Jason Dow <jdowva@gmai.com>
2024-11-22 15:02:00 -0500

    Added keymaps to diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,4 +1,9 @@
 return {
   'https://github.com/sindrets/diffview.nvim',
   lazy = false,
+  config = function ()
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set('n', '<leader>dvo', '<cmd>DiffviewOpen<CR>', opts)
+    vim.keymap.set('n', '<leader>dvc', '<cmd>DiffviewClose<CR>', opts)
+  end
 }
